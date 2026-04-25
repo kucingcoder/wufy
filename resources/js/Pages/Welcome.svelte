db0f3f7d9832f53e6d2b02d83ed20a406955c27d
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2026-04-25 21:51:32 +0700

    update availability status label in Welcome page

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -332,7 +332,7 @@
 
                         <div class="flex items-center gap-2 text-sky-400 text-xs font-bold uppercase tracking-widest">
                             <span class="w-2 h-2 rounded-full bg-sky-500 animate-ping"></span>
-                            Tersedia untuk proyek
+                            Terbuka untuk pengadaan
                         </div>
                     </div>
 
