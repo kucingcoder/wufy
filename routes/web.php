d0d43fccbe9ef644059d99c65e36776648cafe06
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2026-09-17 14:42:14 +0700

    sort projects by is_opensource in web routes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
         'cv_exists' => $profile && $profile->cv_path && Storage::disk('public')->exists($profile->cv_path),
         'education' => Education::orderBy('sort')->get(),
         'experiences' => Experience::orderBy('sort')->get(),
-        'projects' => Project::with('galleries')->orderBy('year', 'desc')->latest()->get(),
+        'projects' => Project::with('galleries')->orderBy('is_opensource', 'asc')->orderBy('year', 'desc')->latest()->get(),
         'skills' => Skill::orderBy('sort')->get(),
         'services' => Service::orderBy('sort')->get(),
         'certificates' => Certificate::orderBy('sort')->get(),
